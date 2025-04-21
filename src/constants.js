ec0f73f937cb3250582006621ce8552141115623
thienbon <kuro113202@gmail.com>
2025-04-21 21:07:09 +0700

    first push

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. My Favorite Language is JavaScript/PHP/Python these days.
+    I've made a couple of projects in these language. I also like C# and C++. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/thienbon" target="_blank">Github</a>!`,
+  "cs-degree": `I am curently studying at Ho Chi Minh University of Science, pursuing a Bachelor's Degree in Information Technology. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">Free code camp</a>, <a href="https://www.youtube.com/@Brackeys" target="_blank">Brackeys</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to take note or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `Contact me at voxuanthien.12c1tn@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of books on my shelves. There is even one in French (I also speak French btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `Gooodbye thank you for look through my profile ;) . ALso thank you Js legend dev for interesting idea and video guide .`,
+};
